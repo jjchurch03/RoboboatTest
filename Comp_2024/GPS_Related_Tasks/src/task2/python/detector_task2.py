f7db08813393d789d0a57ef673a6289917fade70
EKHiigel <90398155+EKHiigel@users.noreply.github.com>
2024-02-06 08:49:32 -0500

    Update detector_task2.py for correct .py script

--- Comp_2024/GPS_Related_Tasks/src/task2/python/detector_task2.py
+++ Comp_2024/GPS_Related_Tasks/src/task2/python/detector_task2.py
@@ -287,7 +287,7 @@ def merge_scripts(weights, img_size, conf_thres=0.4):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='../weights/best.pt', help='model.pt path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default='../weights/bestAugmented.pt', help='model.pt path(s)')
     parser.add_argument('--svo', type=str, default=None, help='optional svo file')
     parser.add_argument('--img_size', type=int, default=416, help='inference size (pixels)')
     parser.add_argument('--conf_thres', type=float, default=0.4, help='object confidence threshold')
