a66055f8b491671d7729744cc122f65ee72be20c
DarkAngelRed <sierragbarbee@gmail.com>
2024-02-06 16:25:49 -0500

    WOOOOOOO

--- Comp_2024/GPS_Related_Tasks/src/task1/python/dirtbag_behavior_v3.py
+++ Comp_2024/GPS_Related_Tasks/src/task1/python/dirtbag_behavior_v3.py
@@ -2,47 +2,32 @@ import rclpy
 from sensor_msgs.msg import NavSatFix
 from threading import Timer
 
-# Global variables to store GPS data and timer object
+# Global variables to store GPS data
 latitude = None
 longitude = None
 file_path = "location_data.txt"
-write_interval = 2  # Update interval in seconds
-timer = None
+write_interval = 10  # Update interval in seconds
 
 def callback(msg):
-    global latitude, longitude, timer
+    global latitude, longitude
     latitude = msg.latitude
     longitude = msg.longitude
 
-    # Schedule writing to file every 5 seconds
+    # Schedule writing to file every 2 seconds
     if latitude is not None and longitude is not None:
-        if timer is None:
-            # Start a new timer only if there's no existing timer
-            timer = Timer(write_interval, write_to_file)
-            timer.start()
-        else:
-            # Otherwise, the timer is already running, do nothing
-            pass
+        Timer(write_interval, write_to_file).start()
 
 def write_to_file():
-    global timer, latitude, longitude
     # Write latitude and longitude to file
     with open(file_path, 'a') as file:
         file.write(f"Latitude: {latitude}, Longitude: {longitude}\n")
         print(f"Latitude: {latitude}, Longitude: {longitude} written to file.")
-    
-    # Reset the timer
-    timer = Timer(write_interval, write_to_file)
-    timer.start()
 
 def ros():
-    global timer
     rclpy.init()
     node = rclpy.create_node('gps_subscriber')
     subscription = node.create_subscription(NavSatFix, 'vectornav/gnss', callback, 10)
     rclpy.spin(node)
-    if timer:
-        timer.cancel()  # Ensure timer is cancelled when exiting
     node.destroy_node()
 
 if __name__ == '__main__':
