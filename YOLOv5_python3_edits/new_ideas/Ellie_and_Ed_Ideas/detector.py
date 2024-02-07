91ce464267b420495139a247a35db3b20e5ce25d
DarkAngelRed <70544228+DarkAngelRed@users.noreply.github.com>
2024-02-07 08:50:35 -0500

    Update detector.py

--- YOLOv5_python3_edits/new_ideas/Ellie_and_Ed_Ideas/detector.py
+++ YOLOv5_python3_edits/new_ideas/Ellie_and_Ed_Ideas/detector.py
@@ -262,7 +262,7 @@ def main():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='../weights/best.pt', help='model.pt path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default='exp13best.pt', help='model.pt path(s)')
     parser.add_argument('--svo', type=str, default=None, help='optional svo file')
     parser.add_argument('--img_size', type=int, default=416, help='inference size (pixels)')
     parser.add_argument('--conf_thres', type=float, default=0.4, help='object confidence threshold')
